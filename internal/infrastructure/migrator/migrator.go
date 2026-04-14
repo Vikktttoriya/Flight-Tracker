1f8b7a41e00dc6a3c59d58978e006da0529cab25
Vikktttoriya <vikktttoriya@gmail.com>
2026-04-14 13:18:55 +0300

    fix: migrator.go

--- internal/infrastructure/migrator/migrator.go
+++ internal/infrastructure/migrator/migrator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
